0ef73a2d922ad1c7d1970e10f1ea48ea541f3d7a
garrison <garrison@service.garrison-shepard.ch-geni-net.geni.uchicago.edu>
2019-05-12 21:23:34 -0500

    feat: ids script complete

---
+++ ids.py
@@ -0,0 +1,29 @@
+import os
+import time
+import mig
+
+def get_est_conn():
+    path = "/users/garrison/est-att-conn.txt"
+    conn_line = len(open(path,'r').readlines())
+    print(conn_line)
+    return conn_line
+
+def listen():
+    while True:
+        os.system("sudo netstat -anp | grep :80 | grep 192.122.236.119 | grep ESTABLISHED > est-att-conn.txt")
+
+        if get_est_conn() > 10:
+            print("UNDER ATTACK")
+            defend_attack()
+            exit(0)
+
+        time.sleep(5)
+
+def defend_attack():
+    mig.migrate() 
+
+def main():
+    listen()
+
+if __name__ == '__main__':
+    main()
